9eedef8fd2d52ea1ccfb9543d23a655e064598f2
Vetik007 <boy0007@ukr.net>
2023-07-10 00:03:36 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
-// import { ToastContainer } from 'react-toastify'; // вспливаючі повідомлення
 import 'react-toastify/dist/ReactToastify.css';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import css from './App.module.css';
-
-// import { MainHeader, SecondaryHeader, Section } from './App.styled';
 
 const App = () => {
   return (
@@ -16,37 +13,7 @@ const App = () => {
       <Filter />
       <ContactList />
     </div>
-
-    // ==================================================
-
-    // <Section>
-    //   <MainHeader>Phonebook</MainHeader>
-    //   <ContactForm />
-    //   <SecondaryHeader>Contacts</SecondaryHeader>
-    //   <Filter />
-    //   <ContactList />
-    //   <ToastContainer position="top-center" />{' '}
-    //   {/* це для вспливаючих повідомлень */}
-    // </Section>
   );
 };
-
-// ==================================================
-// import ContactForm from './ContactForm/ContactForm';
-// import ContactList from './ContactList/ContactList';
-// import Filter from './Filter/Filter';
-// import css from './App.module.css';
-
-// const App = () => {
-//   return (
-//     <div className={css.wrapper}>
-//       <h1 className={css.book}>Phonebook</h1>
-//       <ContactForm />
-//       <h2 className={css.cont}>Contacts</h2>
-//       <Filter />
-//       <ContactList />
-//     </div>
-//   );
-// };
 
 export default App;
