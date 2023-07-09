599d691c1aaee8e1759d89e73efa915dc3bc58b0
Vetik007 <boy0007@ukr.net>
2023-07-10 00:02:17 +0300

    update filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -23,44 +23,7 @@ const Filter = () => {
         onChange={e => dispatch(setContactFilter(e.currentTarget.value))}
       />
     </div>
-
-    // ========================
-    // <Label>
-    //   Find contacts by name
-    //   <Input
-    //     type="text"
-    //     value={filter}
-    //     onChange={event => dispatch(setFilter(event.target.value.trim()))} // діспатчимо екшен
-    //   />
-    // </Label>
   );
 };
-
-// =====================================================
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getContactFilter } from 'redux/selectors';
-// import { setContactFilter } from 'redux/filterSlice';
-// import css from './Filter.module.css';
-
-// const Filter = () => {
-//   const dispatch = useDispatch();
-//   const filter = useSelector(getContactFilter);
-
-//   return (
-//     <div className={css.wrapper}>
-//       <label className={css.label} htmlFor="filter">
-//         Find contacts by name
-//       </label>
-//       <input
-//         className={css.input}
-//         name="filter"
-//         type="text"
-//         id="filter"
-//         value={filter}
-//         onChange={e => dispatch(setContactFilter(e.currentTarget.value))}
-//       />
-//     </div>
-//   );
-// };
 
 export default Filter;
