18882946a1c89cc42bf4cd5c2f50678a3b2ba87b
Vetik007 <boy0007@ukr.net>
2023-07-10 00:09:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,24 +13,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>
 );
-
-// ==============================================
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { store, persistor } from 'redux/store';
-// import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/es/integration/react';
-// import App from 'components/App';
-// import './index.css';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     {/* Передаємо store в Provider */}
-//     <Provider store={store}>
-//       {/* показуємо спінер поки не завантажиться стор */}
-//       <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-//         <App />
-//       </PersistGate>
-//     </Provider>
-//   </React.StrictMode>
-// );
